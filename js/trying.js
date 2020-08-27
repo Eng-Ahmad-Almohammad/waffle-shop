29316e188a1957a7d072eb7116c38c5baaffd8af
Ahmad <almohammedahmed2@gmail.com>
2020-08-27 10:38:04 +0300

    add function

--- js/trying.js
+++ js/trying.js
@@ -1,3 +1,13 @@
 var waffle = prompt("Do you know waffle and crepe");
 var fav = prompt("what do you prefer: waffle, crepe or pancake");
 var number = prompt("how many would you order");
+var hi = prompt()
+function order(){
+    if (number >= 5){
+        alert('welcome in our shop');
+        else {
+            alert('you have to order more');
+        }
+    }
+}
+
